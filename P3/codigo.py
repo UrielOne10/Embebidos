ca976bc571d2b97840f0831c05b351fbb4918c67
Nightmare328 <adjhp.22122004@gmail.com>
2025-10-02 23:16:06 -0600

    LCD (I2C)

--- P3/codigo.py
+++ P3/codigo.py
@@ -1,126 +1,142 @@
-from machine import sleep, SoftI2C, Pin, ADC
+from machine import Pin, SoftI2C, ADC, UART
 from utime import ticks_diff, ticks_ms
 from max30102._init_ import MAX30102, MAX30105_PULSE_AMP_MEDIUM
-import time
-import math
+from lib.i2c_lcd import I2cLcd
+import time, math
 
+# =================== CONFIGURACION LCD ===================
+I2C_ADDR = 0x27
+I2C_NUM_ROWS = 2
+I2C_NUM_COLS = 16
+i2c = SoftI2C(sda=Pin(6), scl=Pin(7), freq=400000)
+lcd = I2cLcd(i2c, I2C_ADDR, I2C_NUM_ROWS, I2C_NUM_COLS)
 
-# Configuración del ADC en GPIO0 (ADC1_CH0)
-adc = ADC(Pin(0))  
-# Configurar la atenuación a 0dB → rango ~0 a 1.1V
-adc.atten(ADC.ATTN_0DB)
-# Configurar resolución de 12 bits (0–4095)
-adc.width(ADC.WIDTH_12BIT)
-
-vector=[]
-tamano=10
-
-VREF = 1.1  # referencia en voltios
+# =================== CONFIGURACION ADC ===================
+adc1 = ADC(Pin(0))  
+adc1.atten(ADC.ATTN_0DB)        # rango ~0 a 1.1V
+adc1.width(ADC.WIDTH_12BIT)     # resolución 12 bits
+VREF = 1.1
 ADC_MAX = 4095
 
-#lectura del ADC y su conversion a temperatura y voltaje
+
+adc2 = ADC(Pin(34))             
+adc2.atten(ADC.ATTN_11DB)       # rango hasta 3.3V
+adc2.width(ADC.WIDTH_12BIT)
+
+# =================== CONFIGURACION UART ===================
+uart = UART(1, baudrate=9600, tx=17, rx=16)
+
+# =================== CONFIGURACION MAX30102 ===================
+sensor = MAX30102(i2c=i2c)
+sensor.setup_sensor()
+sensor.set_pulse_amplitude_red(MAX30105_PULSE_AMP_MEDIUM)
+sensor.set_pulse_amplitude_ir(MAX30105_PULSE_AMP_MEDIUM)
+
+# =================== VARIABLES GLOBALES ===================
+vector = []
+tamano = 10
+ventana_red = []
+ventana_ir = []
+VENTANA_SIZE = 30
+
+estado = "APAGADO"
+activo = None
+
+# =================== FUNCIONES ===================
 def leer_temperatura():
-    lectura = adc.read()              # Guardamos el valor de la lectura en una variable
-    voltaje = (lectura*VREF)/ADC_MAX  # Conversion a voltaje (usando el 1.1 de referencia)
-    temperatura = ((voltaje*1000)/10)    # Conversion a temperatura
-    return temperatura, voltaje,lectura
+    lectura = adc1.read()
+    voltaje = (lectura * VREF) / ADC_MAX
+    temperatura = ((voltaje * 1000) / 10)
+    return temperatura, voltaje, lectura
 
+# =================== FUNCIONES ===================
+def leer_temperatura_lcd():
+    valor_adc = adc2.read()
+    voltaje = (valor_adc / 4095.0) * 3.3
+    temp_c = voltaje * 100
+    return temp_c
 
-    def agregarElemento(self, dato):
-        self.lista.append(dato)
+def mostrar_apagado():
+    lcd.clear()
+    lcd.putstr("   APAGADO   ")
+    lcd.move_to(0, 1)
+    lcd.putstr("Esperando dato")
 
-    def norma2(self):
-        if len(self.lista) >= self.ventana:
-            prom = (math.sqrt(sum(x**2 for x in self.lista))) / len(self.lista)
-            spo2 = 0.12698*prom +79.82  
-            self.lista.clear()
-            return spo2
-        else:
-            return None
-
-
-# ----------------- Programa Principal -----------------
-def main():
-    i2c = SoftI2C(sda=Pin(6), scl=Pin(7), freq=400000)
-    sensor = MAX30102(i2c=i2c)
-
-    if sensor.i2c_address not in i2c.scan():
-        print("Sensor not found.")
-        return
-    elif not sensor.check_part_id():
-        print("I2C device ID not corresponding to MAX30102 or MAX30105.")
-        return
+def mostrar_estado(temp, red=None, spo2=None):
+    lcd.clear()
+    if activo == "F":
+        lcd.putstr("Foco encendido")
+    elif activo == "V":
+        lcd.putstr("Ventilador ON")
     else:
-        print("Sensor connected and recognized.")
+        lcd.putstr("Desconocido")
 
-    sensor.setup_sensor()
-    sensor.set_sample_rate(3200)
-    sensor.set_fifo_average(32)
-    sensor.set_active_leds_amplitude(MAX30105_PULSE_AMP_MEDIUM)
-    
-    #actual_acquisition_rate = int(3200 /4)
-    monitor = HeartRateMonitor()
-    """
-    hr_monitor = HeartRateMonitor(sample_rate=actual_acquisition_rate,
-                                  window_size=int(actual_acquisition_rate * 3))
-                                  """
+    lcd.move_to(0, 1)
+    lcd.putstr("T:{:.1f}C ".format(temp))
+    if red is not None:
+        lcd.putstr("R:{:04d}".format(red))
+    if spo2 is not None:
+        lcd.putstr(" SpO2:{:.1f}".format(spo2))
 
-    ventana_red = []
-    ventana_ir = []
-    ventana_mR = []
-    ventana_mIR= []
-    x=0
-    VENTANA_SIZE = 10  # muestras para SpO2
-    energia = 0
-    valle = 1
-    umbral = 100
-    
-    Energia1=0
-    Energia2=0
-    zeta=0
-    Lista_energia=[]
-    cuadrados=[]
-    suma_cuadrados=0
-    SPO2=0
-    promedio=None
-    print("Iniciando adquisición: RED, IR, BPM, SpO2")
+# =================== MAIN LOOP===================
+try:
+    lcd.clear()
+    mostrar_apagado()
+    print("Iniciando adquisicion: TEMP, RED, SpO2")
 
     while True:
-        #Imprision de la temperatura
-         temp, v, l = leer_temperatura()
-        vector.append(temp*3)
-        if len(vector) == tamano:
-            promedio=math.sqrt(sum(x**2 for  x in vector))/tamano
-            print(" Temp = {:.2f} °C".format( promedio)) #imprimimos promedio de temperatura
-            vector=[] #limpia vector
-        time.sleep(0.1) 
+        # ===== UART: revisar estado =====
+        if uart.any():
+            dato = uart.read(1).decode('utf-8').strip()
+            if dato == "F":
+                activo, estado = "F", "ENCENDIDO"
+            elif dato == "V":
+                activo, estado = "V", "ENCENDIDO"
+            else:
+                activo, estado = None, "APAGADO"
 
+        # ===== Temperatura ADC1  =====
+        temp1, v, l = leer_temperatura()
+        vector.append(temp1 * 3)
+        if len(vector) == tamano:
+            promedio = math.sqrt(sum(x**2 for x in vector)) / tamano
+            print(" Temp1 = {:.2f} °C".format(promedio))
+            vector = []
+
+        # ===== Temperatura ADC2  =====
+        temp2 = leer_temperatura_lcd()
+
+        # ===== MAX30102 =====
         sensor.check()
+        red = None
+        spo2 = None
         if sensor.available():
             red = sensor.pop_red_from_storage()
             ir = sensor.pop_ir_from_storage()
-
-            # Agregar datos a la ventana de SpO2
             ventana_red.append(red)
             ventana_ir.append(ir)
-            
-            monitor.agregarElemento(red)
-            Spo2=monitor.norma2()
-            
+
             if len(ventana_red) > VENTANA_SIZE:
                 ventana_red.pop(0)
                 ventana_ir.pop(0)
-            
-            
-            
-            if(Spo2 is not None):
-                print("RED: {:04d}, Promedio: {:0.2f}".format(red,Spo2))
 
+            # Ejemplo: SpO2 estimado
+            if len(ventana_red) == VENTANA_SIZE:
+                spo2 = sum(ventana_red)/VENTANA_SIZE
+                print("RED: {:04d}, SpO2*: {:.2f}".format(red, spo2))
             else:
-                None
-                print("RED: {:04d}, Promedio: ------".format(red))
-            
+                print("RED: {:04d}".format(red))
 
-if __name__ == "__main__":
-    main()
+        # ===== LCD =====
+        if estado == "APAGADO":
+            mostrar_apagado()
+        else:
+            mostrar_estado(temp2, red, spo2)
+
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    lcd.clear()
+    lcd.putstr("Programa detenido")
+    lcd.backlight_off()
 

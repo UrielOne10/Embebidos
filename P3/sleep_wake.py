6e3c13f30dab064e7a001fa678829bfd3d635c6e
UrielOne10 <uriel.diez@hotmail.es>
2025-10-04 14:22:08 -0600

    Update sleep_wake.py

--- P3/sleep_wake.py
+++ P3/sleep_wake.py
@@ -1,13 +1,38 @@
-import esp32
-from machine import Pin, lightsleep
+# from machine import Pin, lightsleep, DEEPSLEEP,SLEEP
+# from time import sleep
+# # Configurar pin de entrada con wake-up
+# boton = Pin(2, Pin.IN, Pin.PULL_UP,hold = True)
+# boton.irq(trigger=Pin.IRQ_FALLING, wake = SLEEP|DEEPSLEEP)
+# 
+# print("Entrando en light sleep...")
+# sleep(0.1)
+# lightsleep()  # o deepsleep()
+# print("Despertó!")
+#
 
-# Botón en GPIO 4
-btn = Pin(4, Pin.IN, Pin.PULL_UP)
+# from machine import Pin, lightsleep
+# from time import sleep
+# 
+# # Usa un pin adecuado (GPIO4 o GPIO9, NO GPIO2)
+# boton = Pin(4, Pin.IN, Pin.PULL_UP)
+# 
+# def despertar(boton):
+#     print("¡Interrupción detectada, despertando!")
+# 
+# # Configura la interrupción ANTES de entrar en sueño
+# boton.irq(trigger=Pin.IRQ_FALLING, handler=despertar)
+# 
+# print("Entrando en light sleep...")
+# sleep(0.1)
+# lightsleep()
+# print("Despertó del light sleep")
 
-# Configura wake-up por EXT0
-# level=esp32.WAKEUP_ALL_LOW → despierta cuando el pin está LOW
-esp32.wake_on_ext0(pin=btn, level=esp32.WAKEUP_ALL_LOW)
+from machine import Pin, deepsleep, DEEPSLEEP
 
-print("Entrando en lightsleep, presiona el botón para despertar...")
-lightsleep()
-print("¡Despertó por el botón!")
+# Botón conectado de GPIO9 a GND
+boton = Pin(10, Pin.IN, Pin.PULL_UP,hold=True)
+boton.irq(trigger=Pin.IRQ_FALLING, wake=DEEPSLEEP)
+
+print("Entrando en deep sleep...")
+deepsleep()
+print("saliendo")

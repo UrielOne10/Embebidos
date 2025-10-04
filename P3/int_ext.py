6dd9fec02f78146993ed7bc3deb27c3a17638dc9
UrielOne10 <uriel.diez@hotmail.es>
2025-10-03 22:42:21 -0600

    correccion de codigo
    se realizarón correcciones en la logica:
    no hace falta el ticks si solo se está haciendo un toogle de una variable
    se segmento un poco mas el codigo, separando en variables globales configuraciones y vectores de interrupción

--- P3/int_ext.py
+++ P3/int_ext.py
@@ -1,21 +1,19 @@
 from machine import Pin
 import time
 
-pin_uno = Pin(1, Pin.IN, Pin.PULL_DOWN)
-
+# # # # # # variables INT EXT
 estado = False
-last_time = 0
-
+# # # # # # vectores de interrupciones
 def cambiar_estado(pin):
-    global estado, last_time
-    #Time.ticks devuelve el número de milisegundos que han pasado desde que el ESP se encendió
-    now = time.ticks_ms()
-    if time.ticks_diff(now, last_time) > 200: 
-        estado = not estado
-        print(estado)
-        last_time = now
-
+    global estado
+    estado = not estado
+# # # # # # config Interrupciones externas
+pin_uno = Pin(1, Pin.IN, Pin.PULL_DOWN)
 pin_uno.irq(handler=cambiar_estado, trigger=Pin.IRQ_FALLING)
 
+
+# # # # # # APOYO
+salida = Pin(5,Pin.OUT)
+
 while True:
-    time.sleep(0.2)
+    salida.value(estado)    

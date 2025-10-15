7e4ffcd4da092538284e336114f1d7e6cb122bda
Betan2002 <eb72222@gmail.com>
2025-10-15 13:14:44 -0600

    Update Control del switch boucin.py

--- Control del switch boucin.py
+++ Control del switch boucin.py
@@ -1,30 +1,41 @@
-#importar librerias
+#librerias
 from machine import Pin
 import time
 
 # Configurar el botón
 button = Pin(22, Pin.IN, Pin.PULL_DOWN)
 
-# Variables
-estado_anterior = 0  # Estado anterior del botón
+# Variables globales
+estado_anterior = 0
+conteo = 0
+presiones=0
+
+def eliminacion():
+    global estado_anterior, conteo,presiones
+    estado_actual = button.value()
+
+    # Si se presiona (flanco de subida)
+    if estado_actual == 1 and estado_anterior == 0:
+        conteo += 1
+        presiones += 1
+        print(conteo)
+
+    # Si se suelta (flanco de bajada)
+    if estado_actual == 0 and estado_anterior == 1:
+        conteo = 0
+        print(conteo)
+        print("Presiones:", presiones)
+
+    estado_anterior = estado_actual
+
+def int1(pin):  # filtro antirrebote más rápido
+    eliminacion()
+    time.sleep_ms(50) #itiempo limitado para no vizualizar los rebotes
+
+# Configurar interrupción
+button.irq(handler=int1, trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING)
 
 print("Esperando a que el botón se presione...")
 
 while True:
-    
-    #Declarando el estado actual en el quwe se encuentar el boton
-    estado_actual = button.value()
-
-    # Detectar cambio de 0 → 1, comparandolo con el valor anterior y el actual
-    if estado_actual == 1 and estado_anterior == 0:
-        print(1)
-        time.sleep(0.05)  # Anti-rebote (50 ms)
-
-    # Detectar cambio de 1 → 0 (soltado)
-    elif estado_actual == 0 and estado_anterior == 1:
-        print(0)
-        time.sleep(0.05)  # Anti-rebote
-
-    # Actualizar el estado anterior
-    estado_anterior = estado_actual
-
+    None

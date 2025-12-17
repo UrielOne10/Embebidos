4d4fa435edef60f4b18dd7eeea6b6a06473b0fd2
UrielOne10 <uriel.diez@hotmail.es>
2025-12-17 14:57:49 -0600

    Update P1.py

--- P1/P1.py
+++ P1/P1.py
@@ -9,6 +9,7 @@ btns = {
     "b2": {"pin": 4, "pull": Pin.PULL_DOWN, "activo": 1},
     "b3": {"pin": 1, "pull": Pin.PULL_UP,   "activo": 0},
 }
+
 colores = [0,0,0]
 def def_color_fijo():
     colores[0] = 0 if colores[0] else 255
@@ -24,37 +25,31 @@ def def_secuencia():
         if colores[1] >= 255:
             colores[1] = 255
             fase = 1
-
     elif fase == 1: # Y -> G
         colores[0] -= step
         if colores[0] <= 0:
             colores[0] = 0
             fase = 2
-
     elif fase == 2: # G -> C
         colores[2] += step
         if colores[2] >= 255:
             colores[2] = 255
             fase = 3
-
     elif fase == 3: # C -> B
         colores[1] -= step
         if colores[1] <= 0:
             colores[1] = 0
             fase = 4
-
     elif fase == 4: # B -> M
         colores[0] += step
         if colores[0] >= 255:
             colores[0] = 255
             fase = 5
-
     elif fase == 5: # M -> R
         colores[2] -= step
         if colores[2] <= 0:
             colores[2] = 0
             fase = 0
-
     pines["rgb"][0] = colores
     pines["rgb"].write()
     
@@ -64,14 +59,11 @@ def def_sleep():
     pines["rgb"][0] = colores
     pines["rgb"].write()
     lightsleep()
-    
 funciones = {
     "b1": def_color_fijo,
     "b2": def_secuencia,
     "b3": def_sleep
-    }
-
-last_ms = {}
+    }    
 
 def configIO():
     for nombre, cfg in btns.items():
@@ -79,6 +71,7 @@ def configIO():
     pines["rgb"] = neopixel.NeoPixel(Pin(8),1)
 configIO()
 
+last_ms = {}
 def leerBtn():
     for nombre, cfg in btns.items():
         if pines[nombre].value() == cfg["activo"]:
